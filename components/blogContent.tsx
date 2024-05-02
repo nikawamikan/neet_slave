287b021682754010912953055c65378836502453
nikawamikan <nikawamikan@gmail.com>
2024-05-02 18:37:46 +0000

    add: ブログ要素を生成するコンポ作成

---
+++ components/blogContent.tsx
@@ -0,0 +1,31 @@
+import { Blog } from "@/models/microcms"
+import { load } from "cheerio"
+import hljs from "highlight.js"
+import "highlight.js/styles/tokyo-night-dark.css"
+// import "@/styles/blog.css"
+
+export function BlogContent(params: { data: Blog }) {
+    const { data } = params
+    const $ = load(data.content)
+    $("pre code").each((_, elm) => {
+        const className = $(elm).attr("class")
+        const language = className?.replace("language-", "")
+        let result
+        if (language) {
+            try {
+                result = hljs.highlight($(elm).text(), { language })
+            } catch {
+                result = hljs.highlightAuto($(elm).text())
+            }
+        } else {
+            result = hljs.highlightAuto($(elm).text())
+        }
+        $(elm).html(result.value)
+        $(elm).addClass("hljs")
+    })
+    const content = $.html()
+
+    return (
+        <div className="mb-36" dangerouslySetInnerHTML={{ __html: content }} />
+    )
+}
