d6b59a9b720eca5a07853deda27594117e052d6f
nikawamikan <nikawamikan@gmail.com>
2024-05-02 18:36:35 +0000

    add: 文字列日付をよしなにするライブラリ作成

---
+++ lib/date.ts
@@ -0,0 +1,8 @@
+export function toDate(dateString: string): Date {
+    return new Date(dateString)
+}
+
+export function toJpDateStr(dateString: string): string {
+    const date = toDate(dateString)
+    return date.toLocaleDateString('ja-JP')
+}
