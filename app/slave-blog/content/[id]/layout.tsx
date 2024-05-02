4bf56dadcf896326d595ea0233c6eb62b01553c1
nikawamikan <nikawamikan@gmail.com>
2024-05-02 18:38:41 +0000

    add: ブログのレイアウトページ作成

---
+++ app/slave-blog/content/[id]/layout.tsx
@@ -0,0 +1,14 @@
+import "@/styles/blog.css"
+export default function BlogLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+			<div className="inline-block max-w-lg text-left justify-center">
+				{children}
+			</div>
+		</section>
+	);
+}
