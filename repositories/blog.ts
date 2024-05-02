ff3f880398271830ca532dbfb371f45b2f172335
nikawamikan <nikawamikan@gmail.com>
2024-05-02 18:36:03 +0000

    add: microcmsからblog要素を取得するrepositoryの追加

---
+++ repositories/blog.ts
@@ -0,0 +1,45 @@
+import { cmsClient } from "@/lib/microcms"
+import { Blog } from "@/models/microcms";
+
+// 特定のIDのブログ記事を取得する
+export async function fetchBlogById(id: string) {
+    try {
+        const response: Blog = await cmsClient.get({
+            endpoint: 'blog',
+            contentId: id,
+        })
+        return response
+
+    } catch (error) {
+        console.error('Error retrieving blog elements:', error);
+    }
+}
+
+// 特定のユーザーのブログ記事のIDを全て取得する
+async function fetchAllBlogIds(writer: string) {
+    try {
+        const response: Blog[] = await cmsClient.getAllContents({
+            endpoint: 'blog',
+            queries: { filters: `writer[contains]${writer}`, fields: 'id' },
+        })
+        return response.map((blog) => blog.id)
+
+    } catch (error) {
+        console.error('Error retrieving blog ids:', error);
+    }
+
+}
+
+export async function fetchSlaveBlogIds() {
+    const data = await fetchAllBlogIds("nikawamikan")
+    if (data) {
+        return data
+    }
+}
+
+export async function fetchNeetBlogIds() {
+    const data = await fetchAllBlogIds("nantka")
+    if (data) {
+        return data
+    }
+}   
