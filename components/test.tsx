1d6f8f09a6cbf1f49d48b9ca6b9a5bc1c8592a6e
nikawamikan <nikawamikan@gmail.com>
2024-05-02 02:29:50 +0000

    update: spinアニメーションの適用
    使ってみたかった

--- components/test.tsx
+++ components/test.tsx
@@ -1,24 +1,18 @@
-'use client'
+"use client"
 
-import React from 'react'
-import Image from 'next/image' // Import the Image component from next/image
+import React from "react"
+import Image from "next/image" // Import the Image component from next/image
 
 function Test() {
     return (
         <div className="relative h-60 w-full">
             <div
-                className="absolute left-1/2 top-1/2 h-60 w-60 origin-top -translate-x-1/2 
-        -translate-y-1/2 rotate-90 transform transition-all hover:rotate-0"
+                className="
+                absolute left-1/2 top-1/2 h-60 w-60 -translate-x-1/2 -translate-y-1/2 transition-all
+                "
             >
                 <Image
-                    className=""
-                    src="image.png"
-                    width={200}
-                    height={120}
-                    alt="logo"
-                />
-                <Image
-                    className="rotate-90"
+                    className="h-60 w-60 animate-itemspin"
                     src="image.png"
                     width={200}
                     height={120}
