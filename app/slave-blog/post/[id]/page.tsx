b7d7302ab51ea05d31c59109a2d8cc0c29ae2a5c
nikawamikan <nikawamikan@gmail.com>
2024-05-03 11:23:02 +0000

    add: 命名規則に合わせて追加

---
+++ app/slave-blog/post/[id]/page.tsx
@@ -0,0 +1,40 @@
+import { fetchSlaveBlogIds, fetchBlogById } from "@/repositories/blog"
+import { title, subtitle } from "@/components/primitives"
+import { Toc } from "@/components/tocs"
+import { BlogContent } from "@/components/blog-content"
+import { toJpDateStr } from "@/lib/date"
+import { TableOfContents } from "@/components/testtoc"
+
+export async function generateStaticParams() {
+    const ids = await fetchSlaveBlogIds()
+    // ページIDをすべて取得して返す
+    if (!ids) {
+        console.error("Error retrieving blog ids")
+        return []
+    }
+    return ids.map((contentId) => ({ id: contentId }))
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const { id } = params
+    const data = await fetchBlogById(id)
+    if (!data) {
+        return <div>Not Found</div>
+    }
+    return (
+        <>
+            <h1 className={title()}>{data.title}</h1>
+            <div className="mt-8 flex flex-col">
+                <span>公開日: {toJpDateStr(data.publishedAt)}</span>
+                <span>更新日: {toJpDateStr(data.revisedAt)}</span>
+            </div>
+            <br />
+            <span className={subtitle()}>{data.description}</span>
+            <br />
+            {/* <Toc body={data.content} /> */}
+            <br />
+            <BlogContent data={data} />
+            <br />
+        </>
+    )
+}
