893d6cb341e0d59309e42bf5002c58febaa7faf7
nikawamikan <nikawamikan@gmail.com>
2024-05-03 03:02:22 +0000

    fix: メニュー要素修正

--- config/site.ts
+++ config/site.ts
@@ -21,40 +21,6 @@ export const siteConfig = {
 			href: "/about",
 		},
 	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
 	links: {
 		github: "https://github.com/nextui-org/nextui",
 		twitter: "https://twitter.com/getnextui",
