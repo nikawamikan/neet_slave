afc312873815b18b2dd415804057117a830a1a5c
nikawamikan <nikawamikan@gmail.com>
2024-05-03 20:37:22 +0000

    add: tocbotを利用したtoc

---
+++ components/testtoc.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useEffect } from "react"
+import tocbot from "tocbot" // TODO: どうにかする
+import "@/styles/toc.css"
+
+export const TableOfContents: React.FC = () => {
+    useEffect(() => {
+        tocbot.init({
+            tocSelector: ".toc",
+            contentSelector: ".blogContent", // 目次を抽出したい要素のクラス名
+            headingSelector: "h1, h2, h3, h5, h6",
+            scrollSmoothOffset: -60,
+            headingsOffset: 60,
+            scrollSmoothDuration: 300,
+        })
+
+        return () => tocbot.destroy()
+    }, [])
+
+    return (
+        <div className="sticky top-24">
+            <h3 className="font-bold">目次</h3>
+            <div className="toc" />
+        </div>
+    )
+}
