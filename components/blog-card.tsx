e41f05150442ee976d50e69ef9b71740bef66342
nikawamikan <nikawamikan@gmail.com>
2024-05-03 11:29:11 +0000

    add: blogページの一覧で利用するカードコンポの追加

---
+++ components/blog-card.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { Card, CardHeader, CardBody } from "@nextui-org/card"
+import { Image } from "@nextui-org/image"
+import NextLink from "next/link"
+import { siteConfig } from "@/config/site"
+
+export function BlogCard({
+    params,
+}: {
+    params: { id: string; imageUrl: string; title: string; date: string }
+}) {
+    const { id, imageUrl, title, date } = params
+    return (
+        <NextLink href={`${siteConfig.siteItems.slaveBlog}/${id}`} passHref>
+            <Card className="py-4">
+                <CardHeader className="flex-col items-start px-4 pb-0 pt-2">
+                    {/* ここらへんにタグつける */}
+                    <small className="text-default-500">{date}</small>
+                    <h4 className="text-large font-bold">{title}</h4>
+                </CardHeader>
+                <CardBody className="overflow-visible py-2">
+                    <Image
+                        alt="Card background"
+                        className="rounded-xl object-cover"
+                        src={imageUrl}
+                        width={270}
+                    />
+                </CardBody>
+            </Card>
+        </NextLink>
+    )
+}
