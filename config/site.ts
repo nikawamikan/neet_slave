a1e765df69c9b10055ef33883fd7005d3461f7ec
nikawamikan <nikawamikan@gmail.com>
2024-05-04 08:45:48 +0000

    update: サイト名修正

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
+	name: "Neet Slave Blog",
 	description: "Make beautiful websites regardless of your design experience.",
 	navItems: [
 		{
