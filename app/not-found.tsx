054bc00e3b1e77273c716ed13a37264129745aa2
nikawamikan <nikawamikan@gmail.com>
2024-05-04 08:44:34 +0000

    update: ちょっと体裁整えた

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,18 @@
 import { title } from "@/components/primitives"
+import type { Metadata } from "next"
 
+export const metadata: Metadata = {
+    title: "404",
+}
 export default function NotFound() {
     return (
-        <>
-            <title>404</title>
-            <h1 className={title()}>404</h1>
-            <p>私の知る限りそのページは存在しないようです。</p>
-        </>
+        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+            <div className="inline-block max-w-7xl justify-center">
+                <h1 className={title({ color: "violet", fullWidth: true })}>
+                    404
+                </h1>
+            </div>
+            <p>ここはたぶん工事中です。ごめんね</p>
+        </section>
     )
 }
