026fa3a20c68f15af14c4857e26a0312ba12dda7
nikawamikan <nikawamikan@gmail.com>
2024-05-05 07:34:46 +0000

    update: フォント追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import "@/styles/globals.css"
 import { Metadata, Viewport } from "next"
 import { siteConfig } from "@/config/site"
-import { fontSans } from "@/config/fonts"
+import {
+    fontSans,
+    makinas,
+    noteSans,
+    mamelon,
+    kimberalla,
+} from "@/config/fonts"
 import { Providers } from "./providers"
 import { Navbar } from "@/components/navbar"
 import { Link } from "@nextui-org/link"
@@ -35,12 +41,16 @@ export default function RootLayout({
             <head />
             <body
                 className={clsx(
-                    "min-h-screen bg-background font-sans antialiased",
-                    fontSans.variable
+                    "min-h-screen bg-background font-mamelon antialiased",
+                    mamelon.variable,
+                    noteSans.variable,
+                    makinas.variable,
+                    fontSans.variable,
+                    kimberalla.variable
                 )}
             >
                 <Providers
-                    themeProps={{ attribute: "class", defaultTheme: "media" }}
+                    themeProps={{ attribute: "class", defaultTheme: "light" }}
                 >
                     <div className="relative flex h-screen flex-col">
                         <Navbar />
