daf3b65ac1c9c2f60bb590cb76a5f7a38cdb7645
nikawamikan <nikawamikan@gmail.com>
2024-05-06 03:41:12 +0000

    fix: ; 滅ぶべし

--- lib/date.ts
+++ lib/date.ts
@@ -4,5 +4,5 @@ export function toDate(dateString: string): Date {
 
 export function toJpDateStr(dateString: string): string {
     const date = toDate(dateString)
-    return date.toLocaleDateString('ja-JP')
+    return date.toLocaleDateString("ja-JP")
 }
