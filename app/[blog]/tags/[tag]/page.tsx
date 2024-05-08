a1f4751ea99e5c5e70bbef8ed46549906b5e2653
nikawamikan <nikawamikan@gmail.com>
2024-05-08 03:32:09 +0000

    add: タグページ作成

---
+++ app/[blog]/tags/[tag]/page.tsx
@@ -0,0 +1,130 @@
+import { title } from "@/components/primitives"
+import type { Metadata, ResolvingMetadata } from "next"
+import {
+    fetchSlaveBlogListByTag,
+    fetchNeetBlogListByTag,
+    fetchSlaveBlogTags,
+    fetchNeetBlogTags,
+} from "@/services/blog"
+import { BlogCard } from "@/components/blog-card"
+import { Blog } from "@/types/microcms"
+import React from "react"
+import { siteConfig } from "@/config/site"
+import { GenerateOGPImage } from "@/lib/ogpImage"
+
+export async function generateStaticParams() {
+    const slaveBlogTags = await fetchSlaveBlogTags()
+    const neetBlogTags = await fetchNeetBlogTags()
+    const slavePages = slaveBlogTags.map((tag) => ({
+        blog: "slave-blog",
+        tag: tag.id,
+    }))
+    const neetPages = neetBlogTags.map((tag) => ({
+        blog: "neet-blog",
+        tag: tag.id,
+    }))
+
+    return slavePages.concat(neetPages)
+}
+
+export async function generateMetadata({
+    params,
+}: {
+    params: { blog: string; page: string }
+}): Promise<Metadata> {
+    const { blog, page } = params
+    return {
+        title: blog,
+        description: "ブログ一覧ページ",
+        openGraph: {
+            title: blog,
+            description: "ブログ一覧ページ",
+            type: "website",
+            url: `https://neet-slave-blog.com/${blog}/${page}`,
+            images: [
+                GenerateOGPImage(
+                    siteConfig.ogpBaseImages.top,
+                    "ブログ一覧ページ",
+                    "ここは社畜のタグ一覧ページ なんにもいいことないけど 見ていってね",
+                    "SlaveBlog"
+                ),
+            ],
+            siteName: "Neet Slave Blog",
+        },
+    }
+}
+
+function CardList({ blogs }: { blogs: Array<Blog> }) {
+    if (blogs.length === 0) {
+        return <p>記事がありません</p>
+    }
+
+    return (
+        <div className="grid grid-cols-1 gap-4 pt-20 md:grid-cols-2 lg:grid-cols-3">
+            {blogs.map((blog) => (
+                <BlogCard
+                    key={blog.id}
+                    params={{
+                        baseUrl:
+                            blog.writer[0] === "nikawamikan"
+                                ? siteConfig.siteItems.slaveBlog
+                                : siteConfig.siteItems.neetBlog,
+                        id: blog.id,
+                        imageUrl: blog.thumbnail.url,
+                        title: blog.title,
+                        date: blog.publishedAt,
+                    }}
+                />
+            ))}
+        </div>
+    )
+}
+
+function SlaveTop() {
+    return (
+        <div>
+            <h1 className={title()}>社畜ブログ</h1>
+            <p>自称社畜がなんだかんだいろいろ言います</p>
+        </div>
+    )
+}
+
+function NeetTop() {
+    return (
+        <div>
+            <h1 className={title()}>ニートブログ</h1>
+            <p>自称ニートがなんだかんだいろいろ言います</p>
+        </div>
+    )
+}
+
+export default async function BlogPage({
+    params,
+}: {
+    params: { tag: string; blog: string }
+}) {
+    const { blog, tag } = params
+    let blogList: Blog[] = []
+    if (blog !== "neet-blog") {
+        blogList = await fetchSlaveBlogListByTag(tag)
+    } else {
+        blogList = await fetchNeetBlogListByTag(tag)
+    }
+    console.log(blogList)
+
+    // blogList から一意のタグを取得
+    const tags = blogList.map((blog) => blog.tags).flat()
+    // 重複を削除
+    const uniqueTags = Array.from(new Set(tags.map((tag) => tag)))
+    // tagはIDなので、IDからTAGオブジェクトをPOPする
+    const tagObj = uniqueTags.find((tagData) => tagData.id === tag)
+    // uniqueTagsからtagObjを削除
+    const filteredTags = uniqueTags.filter((tagData) => tagData.id !== tag)
+
+    return (
+        <div>
+            {blog === "neet-blog" ? <NeetTop /> : <SlaveTop />}
+            <CardList blogs={blogList} />
+        </div>
+    )
+}
