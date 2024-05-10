b605e99bdbd3e1bc12b63554232e06f3bc696192
nikawamikan <nikawamikan@gmail.com>
2024-05-10 12:31:18 +0000

    add: blogカードをリストで返すコンポ

---
+++ components/blog-card-list.tsx
@@ -0,0 +1,29 @@
+import { Blog } from "@/types/microcms"
+import { BlogCard } from "./blog-card"
+import { siteConfig } from "@/config/site"
+
+export function BlogCardList({ blogs }: { blogs: Array<Blog> }) {
+    if (blogs.length === 0) {
+        return <p>記事がありません</p>
+    }
+
+    return (
+        <div className="grid grid-cols-1 gap-4 pt-20 md:grid-cols-2 lg:grid-cols-3">
+            {blogs.map((blog) => (
+                <BlogCard
+                    key={blog.id}
+                    params={{
+                        baseUrl:
+                            blog.writer[0] === "slave"
+                                ? siteConfig.siteItems.slaveBlog
+                                : siteConfig.siteItems.neetBlog,
+                        id: blog.id,
+                        imageUrl: blog.thumbnail.url,
+                        title: blog.title,
+                        date: blog.publishedAt,
+                    }}
+                />
+            ))}
+        </div>
+    )
+}
