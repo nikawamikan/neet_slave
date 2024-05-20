0bc9e5afef317d81c7ebee56f11baede8858fa42
nikawamikan <nikawamikan@gmail.com>
2024-05-20 14:38:00 +0000

    update: 小さめブログカード追加

--- components/blog-card.tsx
+++ components/blog-card.tsx
@@ -11,11 +11,19 @@ type Prop = {
     imageUrl: string
     title: string
     date: string
+    className?: string
 }
 
-export function BlogCard({ baseUrl, id, imageUrl, title, date }: Prop) {
+export function BlogCard({
+    baseUrl,
+    id,
+    imageUrl,
+    title,
+    date,
+    className,
+}: Prop) {
     return (
-        <NextLink href={`${baseUrl}/${id}`} passHref>
+        <NextLink className={className} href={`${baseUrl}/${id}`} passHref>
             <Card className="py-4">
                 <CardHeader className="flex-col items-start px-4 pb-0 pt-2">
                     {/* ここらへんにタグつける */}
@@ -30,6 +38,47 @@ export function BlogCard({ baseUrl, id, imageUrl, title, date }: Prop) {
                         width={320}
                         height={200}
                     />
+                </CardBody>
+            </Card>
+        </NextLink>
+    )
+}
+
+// 左にサムネイルがあるカード
+export function SmallBlogCard({
+    baseUrl,
+    id,
+    imageUrl,
+    title,
+    date,
+    className,
+}: Prop) {
+    return (
+        <NextLink href={`${baseUrl}/${id}`} passHref>
+            <Card className={`py-4 ${className}`}>
+                <CardBody className="overflow-visible py-2">
+                    <div className="flex flex-row">
+                        <div className="basis-1/4">
+                            <Image
+                                alt="Card background"
+                                className="rounded-xl object-cover"
+                                src={imageUrl}
+                                width={120}
+                                height={120}
+                            />
+                        </div>
+                        <div className="basis-3/4">
+                            <div className="flex-col items-start px-4 pb-0 pt-2">
+                                {/* ここらへんにタグつける */}
+                                <small className="text-default-500">
+                                    {date}
+                                </small>
+                                <h4 className="text-large font-bold">
+                                    {title}
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
                 </CardBody>
             </Card>
         </NextLink>
