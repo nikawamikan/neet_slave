0d1e0e81157baa611b3ead3e7f665163d54dd9d9
nikawamikan <nikawamikan@gmail.com>
2024-05-20 16:57:52 +0000

    update: タグチップのリンクを任意に変更

--- components/tag-chip.tsx
+++ components/tag-chip.tsx
@@ -4,18 +4,22 @@ import NextLink from "next/link"
 
 type Prop = {
     className?: string
-    baseUrl: string
+    baseUrl?: string
     tag: Tag
 }
 
 export function TagChip({ className, baseUrl, tag }: Prop) {
     return (
         <Chip
-            className={`m-2 ${className ? className : ""}`}
+            className={`mr-2 ${className ? className : ""}`}
             color="primary"
             size="md"
         >
-            <NextLink href={`${baseUrl}/${tag.id}`}>{tag.name}</NextLink>
+            {baseUrl ? (
+                <NextLink href={`${baseUrl}/${tag.id}`}>{tag.name}</NextLink>
+            ) : (
+                <div>{tag.name}</div>
+            )}
         </Chip>
     )
 }
