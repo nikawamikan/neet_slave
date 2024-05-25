98a027d6b25ed2594bb54735c5dbdd493711c874
nikawamikan <nikawamikan@gmail.com>
2024-05-25 23:04:38 +0000

    update: APIキャッシュ利用
    APIで取得したデータをもみもみしたあとのキャッシュを作成

--- repositories/blog.ts
+++ repositories/blog.ts
@@ -1,78 +1,21 @@
 import { cmsClient } from "@/lib/microcms"
 import { Blog } from "@/types/microcms"
+import { unstable_cache } from "next/cache"
 
 // すべてのブログ記事を取得する
-export async function fetchAllBlogs(writer: string) {
+export const fetchAllBlogs = unstable_cache(async () => {
     try {
         const response: Blog[] = await cmsClient.getAllContents({
             endpoint: "blog",
-            queries: { filters: `writer[contains]${writer}` },
         })
         return response
     } catch (error) {
         return []
     }
-}
+})
 
-// 特定のIDのブログ記事を取得する
-export async function fetchBlogById(id: string) {
-    try {
-        const response: Blog = await cmsClient.get({
-            endpoint: "blog",
-            contentId: id,
-        })
-        return response
-    } catch (error) {
-        console.error("Error retrieving blog elements:", error)
-    }
-}
-
-// 特定のユーザーのブログ記事のIDを全て取得する
-export async function fetchAllBlogIds(writer: string) {
-    return fetchAllBlogs(writer).then((blogs) => blogs.map((blog) => blog.id))
-}
-
-// ブログのTAG一覧を取得する
-export async function fetchBlogTags(writer: string) {
-    const blogs = await fetchAllBlogs(writer)
-    const tags = blogs.map((blog) => blog.tags).flat()
-    return Array.from(new Set(tags))
-}
-
-// タグごとにブログ記事の一覧を取得する
-export async function fetchBlogListByTag(writer: string, tag_id: string) {
-    try {
-        const response: Blog[] = await cmsClient.getAllContents({
-            endpoint: "blog",
-            queries: {
-                filters: `tags[contains]${tag_id},writer[contains]${writer}`,
-                fields: "id,title,description,thumbnail,publishedAt,writer,tags",
-            },
-        })
-        return response
-    } catch (error) {
-        return []
-    }
-}
-
-// ブログ記事の一覧を取得する
-export async function fetchBlogList(
-    writer: string,
-    limit: number,
-    offset: number
-) {
-    try {
-        const response: { contents: Blog[] } = await cmsClient.get({
-            endpoint: "blog",
-            queries: {
-                filters: `writer[contains]${writer}`,
-                fields: "id,title,description,thumbnail,publishedAt,writer,tags",
-                limit: limit,
-                offset: offset,
-            },
-        })
-        return response.contents
-    } catch (error) {
-        return []
-    }
-}
+// Writerごとのブログ記事を取得する
+export const fetchAllBlogsByWriter = unstable_cache(async (writer: string) => {
+    const blogs = await fetchAllBlogs()
+    return blogs.filter((blog) => blog.writer[0] === writer)
+})
--- services/blog.ts
+++ services/blog.ts
@@ -1,40 +1,97 @@
 import * as repo from "@/repositories/blog"
+import { Blog } from "@/types/microcms"
+import { map } from "lodash"
+import { unstable_cache } from "next/cache"
 
 const SLAVE = "slave"
 const NEET = "neet"
 
 export type writers = typeof SLAVE | typeof NEET
 
-export async function fetchBlogList(
-    writer: writers,
-    limit: number,
-    offset: number
-) {
-    return await repo.fetchBlogList(writer, limit, offset)
-}
+export const fetchBlogList = unstable_cache(
+    async (writer: writers, limit: number, offset: number) => {
+        const blogs = await repo.fetchAllBlogsByWriter(writer)
+        return blogs.slice(offset, offset + limit)
+    }
+)
 
-export async function fetchBlogListByTag(writer: writers, tag_id: string) {
-    return await repo.fetchBlogListByTag(writer, tag_id)
-}
+export const fetchBlogIds = unstable_cache(async (writer: writers) => {
+    return (await repo.fetchAllBlogsByWriter(writer)).map((blog) => blog.id)
+})
 
-export function fetchBlogIds(writer: writers) {
-    return repo.fetchAllBlogIds(writer)
-}
+export const countBlogContent = unstable_cache(async (writer: writers) => {
+    return (await fetchBlogIds(writer)).length
+})
 
-export async function countBlogContent(writer: writers) {
-    return (await repo.fetchAllBlogIds(writer)).length
-}
-
-export async function countPage(writer: writers) {
+export const countPage = unstable_cache(async (writer: writers) => {
     let count = Math.ceil((await countBlogContent(writer)) / 10)
     if (count === 0) count = 1
     return count
+})
+
+const fetchBlogMap = unstable_cache(async () => {
+    const blogs = await repo.fetchAllBlogs()
+    return blogs.reduce(
+        (acc, blog) => {
+            acc[blog.id] = blog
+            return acc
+        },
+        {} as Record<string, Blog>
+    )
+})
+
+export const fetchBlogContent = async (id: string) => {
+    return (await fetchBlogMap())[id]
 }
 
-export async function fetchBlogContent(id: string) {
-    return await repo.fetchBlogById(id)
-}
+export const fetchBlogTags = unstable_cache(async (writer: writers) => {
+    const blogs = await repo.fetchAllBlogsByWriter(writer)
+    const tags = blogs.map((blog) => blog.tags).flat()
+    const uniqueTags = map(tags, "id")
+        .filter((x, i, self) => self.indexOf(x) === i)
+        .map((tag_id) => tags.find((tag) => tag.id === tag_id))
+    return uniqueTags as Blog["tags"]
+})
 
-export async function fetchBlogTags(writer: writers) {
-    return await repo.fetchBlogTags(writer)
-}
+export const fetchBlogTagMap = unstable_cache(async (writer: writers) => {
+    const tags = await fetchBlogTags(writer)
+    return tags.reduce(
+        (acc, tag) => {
+            acc[tag.id] = tag
+            return acc
+        },
+        {} as Record<string, Blog["tags"][0]>
+    )
+})
+
+export const fetchBlogListByTag = unstable_cache(
+    async (writer: writers, tag_id: string) => {
+        const blogs = await repo.fetchAllBlogsByWriter(writer)
+        return blogs.filter((blog) =>
+            blog.tags.some((tag) => tag.id === tag_id)
+        )
+    }
+)
+
+export const fetchBlogListByTagWithLimit = unstable_cache(
+    async (writer: writers, tag_id: string, limit: number, offset: number) => {
+        const blogs = await fetchBlogListByTag(writer, tag_id)
+        return blogs.slice(offset, offset + limit)
+    }
+)
+
+export const countBlogContentWithTag = unstable_cache(
+    async (writer: writers, tag_id: string) => {
+        return (await fetchBlogListByTag(writer, tag_id)).length
+    }
+)
+
+export const countPageWithTag = unstable_cache(
+    async (writer: writers, tag_id: string) => {
+        let count = Math.ceil(
+            (await countBlogContentWithTag(writer, tag_id)) / 10
+        )
+        if (count === 0) count = 1
+        return count
+    }
+)
